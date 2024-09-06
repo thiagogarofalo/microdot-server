56adc70b1b304fc4fc7d8b832c467fe9669d98af
thiagogarofalo <81982850+thiagogarofalo@users.noreply.github.com>
2024-09-06 15:54:58 -0300

    app.py

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,16 @@
+from boot import do_connect
+from microdot import Microdot, send_file
+
+do_connect()
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+async def index(request, dir, file):
+    return send_file("/{}/{}".format(dir, file))
+
+app.run(port=80)
 # Aplicacion del servidor
