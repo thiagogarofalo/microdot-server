3e8f986cd2c60bf294e3abcc4a41964d36ea2843
thiagogarofalo <81982850+thiagogarofalo@users.noreply.github.com>
2024-09-06 15:55:29 -0300

    boot.py

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,15 @@
+def do_connect ():
+    import network
+    from time import sleep
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("Connecting to network...")
+        sta_if.active(True)
+        sta_if.connect("Cooperadora Alumnos", "")
+        while not sta_if.isconnected():
+            print(".", end="")
+            sleep(.25)
+    print("Network config:", sta_if.ifconfig())
+    
+do_connect()
 # Configuracion inicial
